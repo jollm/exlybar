3b2384bdb5da1c2f485736cb4b13c9947e4c5ee3
Jo Gay <jo.gay@mailfence.com>
2021-10-18 18:19:58 -0700

    add a file for useful utility fns

---
+++ exlybar-common.el
@@ -0,0 +1,65 @@
+;;; exlybar-common.el --- Exlybar common fns -*- lexical-binding: t -*-
+
+;; Copyright (C) 2021 Jo Gay <jo.gay@mailfence.com>
+
+;; Author: Jo Gay <jo.gay@mailfence.com>
+;; Version: 0.0.1
+;; Package-Requires: ((xelb "0.18") (emacs "27.1"))
+;; Keywords: window-manager, status-bar, exwm
+
+;; This file is NOT part of GNU Emacs.
+
+;; GNU Emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Part of exlybar.
+
+;; This module provides common functionality for exlybar.
+
+;;; Code:
+
+(require 'xcb)
+
+(defun exlybar--color->pixel (color)
+  "Convert COLOR to PIXEL (index in TrueColor colormap)."
+  (when (and color
+             (eq (x-display-visual-class) 'true-color))
+    (let ((rgb (x-color-values color)))
+      (logior (lsh (lsh (pop rgb) -8) 16)
+              (lsh (lsh (pop rgb) -8) 8)
+              (lsh (pop rgb) -8)))))
+
+(defun exlybar--find-background-color ()
+  "Attempt to guess a reasonable background color."
+  ;; TODO update this after a theme is loaded or changed
+  ;; also, is it possible to get current theme background directly?
+  (frame-parameter (selected-frame) 'background-color))
+
+(defun exlybar--find-foreground-color ()
+  "Attempt to guess a reasonable foreground color."
+  ;; TODO update this after a theme is loaded or changed
+  ;; also, is it possible to get current theme foreground directly?
+  (frame-parameter (selected-frame) 'foreground-color))
+
+(defun exlybar--find-root-window-id ()
+  "Attempt to find a root window."
+  (when exlybar--connection
+    (slot-value (car (slot-value
+                      (xcb:get-setup exlybar--connection) 'roots))
+                'root)))
+
+(provide 'exlybar-common)
+
+;;; exlybar-common.el ends here
