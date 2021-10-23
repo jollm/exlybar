17bb782eec917a71fc1cbbba9fa0ebcd10e53210
Jo Gay <jo.gay@mailfence.com>
2021-10-23 03:06:19 -0700

    add exlybar-pkg.el

---
+++ exlybar-pkg.el
@@ -0,0 +1,22 @@
+(define-package "exlybar" "0.19.1"
+  "Emacs polybar-like thing."
+  '((backlight "1.4")
+    (cl-lib "0.5")
+    (dash "2.1.0")
+    (emacs "27.1")
+    (f "0.20.0")
+    (fontsloth "0.13.0")
+    (s "1.12.0")
+    (volume "20201002.1022")
+    (xelb "0.18"))
+  :keywords
+  '("status-bar" "xcb" "display" "exwm")
+  :authors
+  '(("jo" . "jo.gay@mailfence.com"))
+  :maintainer
+  '("jo" . "jo.gay@mailfence.com"))
+
+;; Local Variables:
+;; no-byte-compile: t
+;; no-native-compile: t
+;;; exlybar-pkg.el ends here
