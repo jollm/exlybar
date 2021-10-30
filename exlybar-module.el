f8a2bf8860da5c2d2c777bbc8fd205153a7619eb
Jo Gay <jo.gay@mailfence.com>
2021-10-29 22:37:35 -0700

    update fontsloth dependency version to 0.15.0
    Fontsloth now uses a font’s default kerning values and also grid fits a little
    better so this should somewhat improve text display for modules.

--- exlybar-module.el
+++ exlybar-module.el
@@ -4,7 +4,7 @@
 
 ;; Author: Jo Gay <jo.gay@mailfence.com>
 ;; Version: 0.20.2
-;; Package-Requires: ((xelb "0.18") (fontsloth "0.14.1") (emacs "27.1"))
+;; Package-Requires: ((xelb "0.18") (fontsloth "0.15.0") (emacs "27.1"))
 ;; Keywords: window-manager, status-bar, exwm
 
 ;; This file is NOT part of GNU Emacs.
